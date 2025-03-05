acdb1cc598c8117239b0aba17edd001acefa194c
Haigsov <haigsouv@my.yorku.ca>
2025-03-04 20:40:36 -0500

    Created player movement

--- player/player.gd
+++ player/player.gd
@@ -1,4 +1,16 @@
 extends CharacterBody2D
 
-func _physics_process(delta: float) -> void:
+
+var speed = 200
+func _physics_process(_delta: float) -> void:
+	player_move()
 	
+	#if Input.is_action_pressed("player 1 move up"):
+		#position.y -= 10 
+	#if Input.is_action_pressed("player 1 move down"):
+		#position.y += 10
+
+func player_move():
+	var dir:float = Input.get_axis("player 1 move up", "player 1 move down")
+	velocity.y = dir * speed
+	move_and_slide()
