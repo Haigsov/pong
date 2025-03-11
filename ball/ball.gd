99f411be96e13d05fdd9d566815cb364429392fd
Haigsov <haigsouv@my.yorku.ca>
2025-03-10 22:15:18 -0400

    Update ball.gd

--- ball/ball.gd
+++ ball/ball.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @onready var ball: CharacterBody2D = $"."
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-var speed:int = 450
+var speed:int = 100
 var dir:Vector2 = Vector2(45.0, 90.0)
 var multiplier:float = 1
 
@@ -11,8 +11,9 @@ var multiplier:float = 1
 	#collision_shape_2d.
 
 func _physics_process(delta: float) -> void:
-	velocity = Vector2(100, -100) * multiplier
+	velocity = (Vector2(speed, -100) * multiplier).rotated(rotation)
 	var collision:KinematicCollision2D = move_and_collide(velocity * delta)
 	if collision:
 		print(collision.get_collider().name)
+		velocity.bounce(collision.get_normal())
 	
