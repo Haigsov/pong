33bab4f11934f5aded755d20d37df6aa882e4429
Haigsov <haigsouv@my.yorku.ca>
2025-03-18 22:52:47 -0400

    Fixed ball movement error
    Move and slide may have been calling the signal multiple times and that may have caused issues.

--- ball/ball.gd
+++ ball/ball.gd
@@ -3,9 +3,9 @@ extends CharacterBody2D
 @onready var ball: CharacterBody2D = $"."
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 @onready var area_2d: Area2D = $Area2D
-var horizontal_speed:int = 100
+var horizontal_speed:int = -100
 var vertical_speed:int
-var dir:Vector2 = Vector2(45.0, 90.0)
+#var dir:Vector2 = Vector2(45.0, 90.0)
 var multiplier:float = 1.25
 
 func _ready() -> void:
@@ -17,18 +17,19 @@ func _ready() -> void:
 	area_2d.body_entered.connect(_on_area_2d_body_entered)
 	
 
-func _physics_process(_delta: float) -> void:
-	move_ball()
+func _physics_process(delta: float) -> void:
+	move_ball(delta)
 	
 
-func move_ball() -> void:
-	#var collision:KinematicCollision2D = move_and_collide(velocity * delta)
-	move_and_slide()
+func move_ball(delta: float) -> void:
+	var collision:KinematicCollision2D = move_and_collide(velocity * delta)
+	#move_and_slide()
 
 func _on_area_2d_body_entered(body: CollisionObject2D) -> void:
 	print("touch")
 	if body is StaticBody2D:
+		print("y = %.2f" % velocity.y)
 		velocity.y = velocity.y * -1
 	elif body is CharacterBody2D:
 		velocity.x = velocity.x * -1 * multiplier
-		print(velocity.x)
+		print("x = %.2f" % velocity.x)
